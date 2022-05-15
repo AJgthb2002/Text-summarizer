1267687d0bb772aace812cf45597bf40e370a9f1
AnanyaJ <joshiananya20@gmail.com>
2022-05-15 13:14:08 +0530

    Improved UI and added feature: copy to clipboard

--- app.py
+++ app.py
@@ -2,16 +2,104 @@ from pywebio.input import *
 from pywebio.output import *
 from pywebio import start_server, session
 from summarizer import *
+import pyperclip as pc
+
+header_img = open('images/nutshell_header.png', 'rb').read()
+welcome_img= open('images/Nutshell.jpg', 'rb').read()
+
+def menu():
+    
+    input_choice = input_group('Get Started', [
+    actions('', [ 
+        {'label': 'Upload a file', 'value': 1}, 
+        {'label': 'Enter text', 'value': 2}, 
+        ], name='action', help_text=None),
+    ])
+
+    user_inp= input_choice["action"]
+    if user_inp==1:
+        clear("main")
+        with use_scope("ROOT",clear=True):
+            put_image(header_img, width='100%', height='100%', position=0)
+        f = file_upload("Upload a text file", accept=".txt", max_size='10M',required=True) 
+        l= input("Length of summary (No. of sentences) ", type=NUMBER)                 
+        userfile= open('datafiles/'+f['filename'], 'wb').write(f['content'])  
+        summary= generate_summary_from_file('datafiles/'+f['filename'], top_n=l)
+        wordcloud_img = open('./wordcloud.jpg', 'rb').read()
+        clear("main")
+        with use_scope("main",clear=True):
+            put_image(wordcloud_img, width='100%', height='100%')
+            put_html(f"<h1> Summary: </h1><p>{summary}</p>")
+        
+        selected_opt=None
+        while(selected_opt!=3):
+            summary_choice = input_group('', [
+            actions('', [ 
+                {'label': 'Copy to clipboard', 'value': 1}, 
+                {'label': 'Share wordcloud', 'value': 2}, 
+                {'label': 'Back', 'value': 3}, 
+                ], name='action', help_text=None),
+            ])
+            selected_opt=summary_choice["action"]
+            if selected_opt==1:
+                pc.copy(summary)
+                with use_scope("main"):
+                    put_success("Copied to Clipboard!")
+            elif selected_opt==2:
+                pass 
+            elif selected_opt==3:
+                clear("main")
+                clear("ROOT")
+        return 
+
+    if user_inp==2:
+        clear("main")
+        with use_scope("ROOT",clear=True):
+            put_image(header_img, width='100%', height='100%', position=0)
+        text= textarea(label="Paste your text here", rows=8, minlength=20)
+        l= input("Length of summary (No. of sentences) ", type=NUMBER)        
+        summary= generate_summary_from_text(text, top_n=l)
+        wordcloud_img = open('./wordcloud.jpg', 'rb').read()
+        clear("main")
+        with use_scope("main",clear=True):
+            put_image(wordcloud_img, width='100%', height='100%')
+            put_html(f"<h1> Summary: </h1><p>{summary}</p>")
+        
+        selected_opt=None
+        while(selected_opt!=3):
+            summary_choice = input_group('', [
+            actions('', [ 
+                {'label': 'Copy to clipboard', 'value': 1}, 
+                {'label': 'Share wordcloud', 'value': 2}, 
+                {'label': 'Back', 'value': 3}, 
+                ], name='action', help_text=None),
+            ])
+            selected_opt=summary_choice["action"]
+            if selected_opt==1:
+                pc.copy(summary)
+                with use_scope("main"):
+                    put_success("Copied to Clipboard!")
+            elif selected_opt==2:
+                pass 
+            elif selected_opt==3:
+                clear("main")
+                clear("ROOT")
+        return        
+
 
 def main():  # PyWebIO application function
-    session.set_env(title='Summarizer', output_animation=False)
-    put_html("<h1> Our Awesome Text Summarizer Tool </h1>")
+    session.set_env(title='Nutshell', output_animation=False)
+    # put_html("<h1> Nutshell Text Summarizer Tool </h1>")
     
-    text= textarea(label="Paste your text here", rows=8, minlength=20)
-    summary= generate_summary_from_text(text)
-    wordcloud_img = open('./wordcloud.png', 'rb').read()
-    put_image(wordcloud_img, width='100%', height='100%')
-    put_html(f"<h1> Summary: </h1><p>{summary}</p>")
+    while(True):
+        with use_scope("main",clear=True):
+            put_image(src=welcome_img, width='120%', height='390px')  #put welcome screen
+        menu()
+        
+
+    
+    
+    
        
     
     
--- summarizer.py
+++ summarizer.py
@@ -5,16 +5,16 @@ from nltk.cluster.util import cosine_distance
 import numpy as np
 import networkx as nx
 from wordcloud import WordCloud
-import matplotlib.pyplot as plt
+
+#import matplotlib.pyplot as plt
  
 def read_article(file_name):
-    file = open(file_name, "r")
-    filedata = file.readlines()
-    article = filedata[0].split(". ")
+    f = open(file_name, "r")
+    filedata = f.read()
+    article = filedata.split(". ")
     sentences = []
 
     for sentence in article:
-        print(sentence)
         sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
     sentences.pop() 
     
@@ -59,7 +59,7 @@ def build_similarity_matrix(sentences, stop_words):
     return similarity_matrix
 
 
-def generate_summary(file_name, top_n=5):
+def generate_summary_from_file(file_name, top_n=5):
     stop_words = stopwords.words('english')
     summarize_text = []
 
@@ -75,14 +75,18 @@ def generate_summary(file_name, top_n=5):
 
     # Step 4 - Sort the rank and pick top sentences
     ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)    
-    print("Indexes of top ranked_sentence order are ", ranked_sentence)    
+    # print("Indexes of top ranked_sentence order are ", ranked_sentence)    
 
     for i in range(top_n):
       summarize_text.append(" ".join(ranked_sentence[i][1]))
 
     # Step 5 - Offcourse, output the summarize texr
-    print("Summarize Text: \n", ". ".join(summarize_text))
 
+    word_cloud = WordCloud(collocations = False, background_color = 'white').generate(". ".join(summarize_text))
+    word_cloud.to_file('wordcloud.jpg')
+    return(". ".join(summarize_text)) 
+
+    
 def generate_summary_from_text(text, top_n=5):
     stop_words = stopwords.words('english')
     summarize_text = []
@@ -90,7 +94,6 @@ def generate_summary_from_text(text, top_n=5):
     sentences = []
 
     for sentence in article:
-        # print(sentence)
         sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
     sentences.pop() 
 
@@ -106,7 +109,7 @@ def generate_summary_from_text(text, top_n=5):
 
     # Step 4 - Sort the rank and pick top sentences
     ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)    
-    print("Indexes of top ranked_sentence order are ", ranked_sentence)    
+    # print("Indexes of top ranked_sentence order are ", ranked_sentence)    
 
     for i in range(top_n):
       summarize_text.append(" ".join(ranked_sentence[i][1]))
@@ -117,7 +120,3 @@ def generate_summary_from_text(text, top_n=5):
     word_cloud.to_file('wordcloud.jpg')
     return(". ".join(summarize_text))    
 
-# generate_summary("msft.txt", 2)
-
-def generate_dummy_summary(text):
-    return text
